ce098f71c18f1772cbf9c9ff02b673c82c660243
whitefeatherbpo <towhid123hossain@gmail.com>
2020-06-14 11:36:51 -0400

    get all names

--- com.mavenWindyTownNice/src/test/java/dropDownBox/DropDownGetAllNames.java
+++ com.mavenWindyTownNice/src/test/java/dropDownBox/DropDownGetAllNames.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
-public class DropDown {
+public class DropDownGetAllNames {
 
 	public static void main(String[] args) {
 		
